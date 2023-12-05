4030bef6560ba0284f64c1a1bdeea5a60b7cba75
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-12-05 01:56:35 +0100

    Reformat

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,7 @@ import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [
-    dts({
-      insertTypesEntry: true,
-    }),
-  ],
+  plugins: [dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: resolve(__dirname, "lib/math/modulo.ts"),
