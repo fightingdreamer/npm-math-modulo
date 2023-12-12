2ef2ee52d16f5128437759261e43a2e71651e9d3
Patryk Stępień <fighting.dreamer.0@gmail.com>
2023-12-12 17:31:59 +0100

    Add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+  root: true,
+};
